132845b48b4e449268ab41c466834cf7fc2ba762
berezkin88 <berezkin05@gmail.com>
2020-05-21 17:28:21 +0300

    implement custom CrudService, closes #10

--- pet-clinic-data/src/main/java/person/birch/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/person/birch/petclinic/services/VetService.java
@@ -1,15 +1,6 @@
 package person.birch.petclinic.services;
 
-import person.birch.petclinic.model.Owner;
 import person.birch.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
