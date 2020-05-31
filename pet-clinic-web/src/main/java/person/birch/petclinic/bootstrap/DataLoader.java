51d07e0808806f742cfe3caa67ba56e5cbd0782e
berezkin88 <berezkin05@gmail.com>
2020-05-31 12:57:55 +0300

    add PetTypeService to Bootstrap (startup), closes #32

--- pet-clinic-web/src/main/java/person/birch/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/person/birch/petclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package person.birch.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import person.birch.petclinic.model.Owner;
+import person.birch.petclinic.model.PetType;
 import person.birch.petclinic.model.Vet;
 import person.birch.petclinic.services.OwnerService;
+import person.birch.petclinic.services.PetTypeService;
 import person.birch.petclinic.services.VetService;
 
 @Component
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
