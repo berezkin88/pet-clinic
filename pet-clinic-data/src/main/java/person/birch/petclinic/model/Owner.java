2c039b395ae7680a0c04b2fc41038b4385d703d6
berezkin88 <berezkin05@gmail.com>
2020-05-31 12:45:55 +0300

    update Owner entity with new fields, closes #28

--- pet-clinic-data/src/main/java/person/birch/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/person/birch/petclinic/model/Owner.java
@@ -5,11 +5,11 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-//    private String address;
-//    private String city;
-//    private String telephone;
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets = new HashSet<>();
-//
+
 //    public Owner(String address, String city, String telephone) {
 //        this.address = address;
 //        this.city = city;
@@ -18,30 +18,30 @@ public class Owner extends Person {
 //
 //    public Owner() {
 //    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-//
-//    public String getCity() {
-//        return city;
-//    }
-//
-//    public void setCity(String city) {
-//        this.city = city;
-//    }
-//
-//    public String getTelephone() {
-//        return telephone;
-//    }
-//
-//    public void setTelephone(String telephone) {
-//        this.telephone = telephone;
-//    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
