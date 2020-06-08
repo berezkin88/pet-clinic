515ffeed05f945458ca594d23ad41955427bec18
Alexander <berezkin05@gmail.com>
2020-06-08 13:15:05 +0300

    Create Spring Data JPA for PetType. Closed #43

---
+++ pet-clinic-data/src/main/java/person/birch/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package person.birch.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import person.birch.petclinic.model.PetType;
+import person.birch.petclinic.repositories.PetTypeRepository;
+import person.birch.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * @author Aleksandr Beryozkin
+ */
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return (Set<PetType>) petTypeRepository.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
