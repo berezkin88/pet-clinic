6e02aa7b730747e187e4d21f40289ee025ca6795
Alexander <berezkin05@gmail.com>
2020-06-13 13:17:11 +0300

    Create Visit Map Service. Closed #47

---
+++ pet-clinic-data/src/main/java/person/birch/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package person.birch.petclinic.services;
+
+import person.birch.petclinic.model.Visit;
+
+/**
+ * @author Aleksandr Beryozkin
+ */
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
